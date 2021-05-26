90de88a4f815acf11d098202cc14519563b204c0
Rachel Hendricks <rachelhen92@gmail.com>
2021-05-26 11:59:09 -0600

    Add onClick to button

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Card.css';
 
-const Card = ({ name, date, time, number }) => {
+const Card = ({ name, date, time, number, id, deleteReservation, deleteReservationFromApi }) => {
   return (
     <article className='card'>
       <h2>{name}</h2>
       <p>{date}</p>
       <p>{time} pm</p>
       <p>Number of guests: {number}</p>
-      <button>Cancel</button>
+      <button onClick={() => deleteReservation(id)}>Cancel</button>
     </article>
   )
 }
